5a8941e5a6d3a1c44b0c72b88384265161920839
stujo <stujo@imagescript.com>
2014-10-02 21:53:21 -0700

    Prompt user for url or filename

--- pixster.rb
+++ pixster.rb
@@ -1,28 +1,42 @@
 require 'typhoeus'
 require 'nokogiri'
 
+print "What file or url? : "
 
-response = Typhoeus.post(
-  "http://picascii.com/upload.php",
-  {
-    body: {
-      'MAX_FILE_SIZE' => '1048576',
-      'quality' => '3',
-      'size' => '5',
-      imageupload: File.open('/Users/stuart/Desktop/220px-Nyan_cat_250px_frame.PNG',"r")
-    },
-    cookiefile: "./cookies",
-    cookiejar: "./cookies",
-    followlocation: true
+filename_or_url = $stdin.gets.chomp
+
+unless filename_or_url.nil?
+
+  puts "Processing #{filename_or_url}"
+
+  payload  = {
+    'MAX_FILE_SIZE' => '1048576',
+    'quality' => '3',
+    'size' => '5',
   }
-)
 
-html_doc = Nokogiri::HTML(response.body)do |config|
-  config.strict.nonet
+  if filename_or_url.include? "://"
+    payload[:url] = filename_or_url
+  else
+    payload[:imageupload] = File.open(filename_or_url,"r")
+  end
+
+  response = Typhoeus.post(
+    "http://picascii.com/upload.php",
+    {
+      body: payload,
+      cookiefile: "./cookies",
+      cookiejar: "./cookies",
+      followlocation: true
+    }
+  )
+
+  html_doc = Nokogiri::HTML(response.body)do |config|
+    config.strict.nonet
+  end
+
+
+  textarea = html_doc.css("textarea").first
+
+  puts textarea.text
 end
-
-
-textarea = html_doc.css("textarea").first
-
-puts textarea.text
-
